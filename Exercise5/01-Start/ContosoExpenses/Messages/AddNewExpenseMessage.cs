9a22b2624e26a5d71aa0cce8856624f0652567e2
Matteo Pagani <matteo.pagani@microsoft.com>
2019-04-06 14:49:50 +0200

    Added Timeline support to Exercise 5

---
+++ Exercise5/01-Start/ContosoExpenses/Messages/AddNewExpenseMessage.cs
@@ -0,0 +1,11 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContosoExpenses.Messages
+{
+    public class AddNewExpenseMessage: MessageBase
+    {
+    }
+}
