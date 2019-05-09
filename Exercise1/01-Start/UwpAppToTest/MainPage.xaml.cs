84ef1289c43ff6be20a03e50e2e36ea0de55dc05
ridomin <ridomin@live.com>
2019-05-09 11:04:18 -0700

    using acrylic brush

--- Exercise1/01-Start/UwpAppToTest/MainPage.xaml.cs
+++ Exercise1/01-Start/UwpAppToTest/MainPage.xaml.cs
@@ -13,22 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace CamaronDe
+namespace UwpAppToTest
 {
-    public sealed partial class ModernControlsDemo : UserControl
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
     {
-        public ModernControlsDemo()
+        public MainPage()
         {
             this.InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            datePicker1.SelectedDate = DateTime.Now;
-            timePicker1.SelectedTime = DateTime.Now.TimeOfDay;
-            textBlock1.Text = typeof(string).Assembly.Location;
         }
     }
 }
