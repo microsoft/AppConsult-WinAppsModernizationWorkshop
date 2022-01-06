465223895f65a338e29a19831a963aefdfbec568
Kazuki Ota <kaota@microsoft.com>
2022-01-06 17:12:35 +0900

    Change exception type for ConvertBack

--- src/ContosoExpenses/Converters/CostToHeightConverter.cs
+++ src/ContosoExpenses/Converters/CostToHeightConverter.cs
@@ -20,7 +20,7 @@ namespace ContosoExpenses.Converters
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
     }
 }
